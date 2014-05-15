ae461143789bf867233034db7551311340508d4a
Greg Minshall <minshall@acm.org>
2014-05-15 18:54:53 +0100

    *** empty log message ***

--- testavg.c
+++ testavg.c
@@ -1,18 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DUMP() fprintf(stderr, "nvals = %d, avg32 = %d, avg32/nvals = %d, avg16 = %d, val = %d\n", nvals, avg32, avg32/nvals, avg16, val);
+#define DUMP() fprintf(stderr, "nvals = %d, oavg16 = %d, val = %d, avg32 = %d, avg32/nvals = %d(%g), (val-oavg16)/nvals = %d(%g), oavg16+((val-oavg16)/nvals) = %d(%g), avg16 = %d\n", nvals, oavg16, val, avg32, avg32/nvals, (avg32*1.0)/(nvals*1.0), (val-oavg16)/nvals, ((val*1.0)-(oavg16*1.0))/(nvals*1.0), oavg16+((val-oavg16)/nvals), (oavg16*1.0)+(((val*1.0)-(oavg16*1.0))/(nvals*1.0)), avg16);
 
 int
 main() {
-    int avg32 = 0, avg16 = 0;
+    int avg32 = 0, avg16 = 0, navg16, oavg16;
     int val, nvals = 0;
 
     while (scanf(" %x", &val) == 1) {
         nvals++;
         avg32 += val;
+        oavg16 = avg16;
         if (nvals) {
-            avg16 = (avg16 + ((val-avg16)/nvals))&0xffff;
+            navg16 = (avg16 + ((val-avg16)/nvals))&0xffff;
+            /* fix up low order */
+            if ((((val-avg16)/nvals)*nvals) != (val-avg16)) {
+                if (val-avg16 > 0) {
+                } else {
+                    navg16--;
+                }
+            }
+            avg16 = navg16;
         } else {
             avg16 = val&0xffff;
         }
