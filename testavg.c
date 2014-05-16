79aec8db7e1ae23edbb1ec15009519f749b3e629
Greg Minshall <minshall@acm.org>
2014-05-16 06:56:55 +0100

    *** empty log message ***

--- testavg.c
+++ testavg.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define DUMP() fprintf(stderr, "nvals = %d, oavg16 = %d, val = %d, avg32 = %d, rnd(avg32/nvals) = %d(%g), (val-oavg16)/nvals = %d(%g), oavg16+((val-oavg16)/nvals) = %d(%g), avg16 = %d\n", nvals, oavg16, val, avg32, (avg32+(nvals/2))/nvals, (avg32*1.0)/(nvals*1.0), (val-oavg16)/nvals, ((val*1.0)-(oavg16*1.0))/(nvals*1.0), oavg16+((val-oavg16)/nvals), (oavg16*1.0)+(((val*1.0)-(oavg16*1.0))/(nvals*1.0)), avg16);
+#define DUMP() fprintf(stderr, "nvals = %d, oavg16 = %d, val = %d, avg32 = %d, rnd(avg32/nvals) = %d(%g), (val-oavg16)/nvals = %d(%g), oavg16+((val-oavg16)/nvals) = %d(%g), accerr = %d, avg16 = %d\n", nvals, oavg16, val, avg32, (avg32+(nvals/2))/nvals, (avg32*1.0)/(nvals*1.0), (val-oavg16)/nvals, ((val*1.0)-(oavg16*1.0))/(nvals*1.0), oavg16+((val-oavg16)/nvals), (oavg16*1.0)+(((val*1.0)-(oavg16*1.0))/(nvals*1.0)), accerr, avg16);
 
 int
 main() {
-    int avg32 = 0, avg16 = 0, oavg16;
-    int val, nvals = 0, tmp;
+    int avg32 = 0, avg16 = 0, nvals = 0, accerr = 0;
+    int val, tmp, oavg16;;
 
     while (scanf(" %x", &val) == 1) {
         nvals++;
@@ -15,15 +15,19 @@ main() {
         if (nvals) {
             avg16 = (oavg16 + ((val-oavg16)/nvals))&0xffff;
             /* fix up low order */
-            tmp = (((val-oavg16)/nvals)*nvals)-(val-oavg16);
-            if (tmp != 0) {
-                fprintf(stderr, "tmp = %d\n", tmp);
-                if (abs(tmp) > (nvals/2)) {
-                    if (val-oavg16 > 0) {
-                        avg16++;
-                    } else {
-                        avg16--;
-                    }
+            accerr += (((val-oavg16)/nvals)*nvals)-(val-oavg16);
+            if (abs(accerr) >= nvals) {
+                fprintf(stderr, "accerr = %d\n", accerr);
+                if (accerr > 0) {
+                    avg16++;
+                    accerr -= nvals;
+                } else {
+                    avg16--;
+                    accerr += nvals;
+                }
+                if (accerr >= nvals) {
+                    fprintf(stderr, "accerr %d >= nvals %d\n", accerr, nvals);
+                    exit(3);
                 }
             }
         } else {
@@ -37,7 +41,7 @@ main() {
         if (avg16 != ((avg32+(nvals/2))/nvals)) {
             fprintf(stderr, "discrepancy: ");
             DUMP();
-            exit(2);
+//            exit(2);
         }
         DUMP();
     }
