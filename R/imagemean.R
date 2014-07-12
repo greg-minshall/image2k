0d49700bd2e38d8cb60bb757964aec4a1d0edd6b
Greg Minshall <minshall@acm.org>
2014-07-12 07:20:30 +0300

    s/read.image2k/p2k.read/g s/write.image2k/p2k.write/g
    (all for the hope that some day i'll have dt2k.read, i2k.read, et al.,
    for data.table, image, etc., structures.)

--- R/imagemean.R
+++ R/imagemean.R
@@ -23,7 +23,7 @@ imagemean <- function(file, output=NULL, ...) {
 
   nfiles = 0;
   for (f in files) {
-    pm <- read.image2k(f, ...);
+    pm <- p2k.read(f, ...);
     if (nfiles == 0) {
       sum <- pm;
     } else {
@@ -42,6 +42,6 @@ imagemean <- function(file, output=NULL, ...) {
   if (is.null(output)) {
     return(sum);
   } else {
-    write.image2k(output, pm);
+    p2k.write(output, pm);
   }
 }
